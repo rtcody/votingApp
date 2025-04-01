ca2432aa30c78e16a737669794d6b899ad2a2006
rtcody <rylan.cody@wsu.edu>
2025-04-01 13:18:56 -0700

    SQL TABLES UPDATED, backend api route added

--- backend/app.py
+++ backend/app.py
@@ -1,10 +1,33 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
+import psycopg2
+
 
 app = Flask(__name__)  
 
 @app.route('/')
 def blank():   
     return "Welcome to VotingApp Backend"
+
+# Create a global connection to be reused
+def get_db_connection():
+    conn = psycopg2.connect(
+        dbname='mydb',
+        user='postgres',
+        password='mysecretpassword',
+        host='localhost',
+        port='5433'
+    )
+    return conn
+
+@app.route('/users')
+def users():
+    conn = get_db_connection()
+    cursor = conn.cursor()
+    cursor.execute('SELECT * FROM users')
+    users = cursor.fetchall()
+    cursor.close()
+    conn.close()  # Close the connection after use
+    return {'users': users}
 
 if __name__ == '__main__':
     app.run(debug=True)
